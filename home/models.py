80a74b4526cdc3d4a633bcf84e822fdd9cc482af
mashmool0 <ap4games2017@gmail.com>
2024-05-03 00:50:18 +0330

    implement basic todo list

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Tasks(models.Model):
+    Taskname = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.Taskname[:50]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tasks
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'home/home.html', context={})
+    if request.method == 'POST':
+        if 'task' in request.POST:
+            taskname = request.POST['task']
+            Tasks.objects.create(Taskname=taskname)
+            return redirect('/')
+        if 'delete_task' in request.POST:
+            if Tasks.objects.count() > 0 :
+                Tasks.objects.get(id=request.POST['delete_task']).delete()
+
+
+    tasks_list = Tasks.objects.all()[::-1]
+    return render(request, 'home/home.html', context={"task_list": tasks_list})
